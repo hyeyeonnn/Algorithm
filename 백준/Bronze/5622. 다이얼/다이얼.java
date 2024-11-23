b5366c806fd3bdeb3980c02d4651fdc9633e7f5d
hyeyeonnn <79998154+hyeyeonnn@users.noreply.github.com>
2024-11-23 16:52:22 +0900

    [Bronze II] Title: 다이얼, Time: 164 ms, Memory: 17564 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();   // 단어를 입력한다.
+        int cnt = 0;    // 기본적으로 2 초 이후에 시작하기 때문에
+
+        for(int i=0; i<a.length(); i++) {
+            switch(a.charAt(i)) {
+                case 'A' : case 'B' : case 'C':
+                    cnt += 3;
+                    break;
+                case 'D' : case 'E' : case 'F':
+                    cnt += 4;
+                    break;
+                case 'G' : case 'H' : case 'I':
+                    cnt += 5;
+                    break;
+                case 'J' : case 'K' : case 'L':
+                    cnt += 6;
+                    break;
+                case 'M' : case 'N' : case 'O':
+                    cnt += 7;
+                    break;
+                case 'P' : case 'Q' : case 'R': case 'S':
+                    cnt += 8;
+                    break;
+                case 'T' : case 'U' : case 'V':
+                    cnt += 9;
+                    break;
+                case 'W' : case 'X' : case 'Y' : case 'Z':
+                    cnt += 10;
+                    break;
+            }
+        }
+        System.out.println(cnt);
+
+    }
+
+}
